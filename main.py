34185e7de07831ebccc25e16f6e24bb77c7b3f8f
ignacyr <79996325+ignacyr@users.noreply.github.com>
2022-04-05 19:14:01 +0200

    added "if __name__ == '__main__':  main()"

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from SVC_classification import svc_classification
 from decision_tree_classification import decision_tree_classification
 from numpy import load
 
-resolution = 100  # don't set to more than 100
-import_images(resolution)
 
-categories = load('categories.npy')
-images = load('images.npy')
-test_samples = load('test_samples_img.npy')
+def main():
+    resolution = 100  # don't set to more than 100
+    # import_images(resolution)
 
-decision_tree_classification(categories, images, test_samples, resolution)
-svc_classification(categories, images, test_samples, resolution)
-mlp_classification(categories, images, test_samples, resolution)
+    categories = load('categories.npy')
+    images = load('images.npy')
+    test_samples = load('test_samples_img.npy')
+
+    decision_tree_classification(categories, images, test_samples, resolution)
+    svc_classification(categories, images, test_samples, resolution)
+    mlp_classification(categories, images, test_samples, resolution)
+
+
+if __name__ == '__main__':
+    main()
